050b8adb3d251f480e076f9c309a26f03fbd7e77
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-12-09 18:22:59 -0300

    feat(patient): implement doctorcard at doctorList

---
+++ src/components/Doctor/DoctorCard/DoctorCard.tsx
@@ -0,0 +1,78 @@
+import { IonCard, IonCardContent, IonThumbnail, IonImg, IonButton, IonIcon, useIonAlert, useIonToast } from '@ionic/react'
+import { heartOutline, chatbubbleOutline, calendarOutline } from 'ionicons/icons'
+import React, { useState } from 'react'
+import { Doctor } from '../../../types';
+
+const slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+  };
+
+interface DoctorCardProps {
+  doctor: Doctor;
+}
+ 
+  
+function DoctorCard({ doctor }: DoctorCardProps) {
+  const [change, setChange] = useState<boolean>(false);
+  const [_class, setClass] = useState<string>("flex hidden");
+  const [presentAlert] = useIonAlert();
+  const [handlerMessage, setHandlerMessage] = useState("");
+  const [roleMessage, setRoleMessage] = useState("");
+  const [present] = useIonToast();
+  const presentToast = () => {
+    
+    present({
+      message: "Consulta cancelada com sucesso",
+      duration: 1500,
+      position: "top",
+    });
+  };
+
+  const showChat = () => {
+    setChange(!change);
+    if (change === true) {
+      setClass("flex");
+    } else {
+      setClass("flex hidden");
+    }
+  };
+  return (
+    <div className="container" onClick={showChat} >
+      <IonCard className="bd-20 card" >
+        <IonCardContent className="flex justify-between">
+          <IonThumbnail slot="start">
+            <img
+              className="min-w-[80px]"
+              alt="Pic-Doctor"
+              src="./assets/avatar/Pic-Doctor.png"
+              />
+          </IonThumbnail>
+
+          <div className="flex flex-col gap-1 ml-11">
+            <span className="text-black font-bold">{doctor.nameDoctor}</span>
+            <p className="font-normal">{doctor.speciality}</p>
+            <span className="font-medium">98 Avaliações</span>
+          </div>
+          <IonButton fill="clear">
+            <IonIcon slot="icon-only" icon={heartOutline}></IonIcon>
+          </IonButton>
+        </IonCardContent>
+        <div className="flex flex-row justify-center items-center">
+          <div className={_class}>
+            <IonButton className="text-xs w-max">
+              ABRIR CHAT
+              <IonIcon slot="start" icon={chatbubbleOutline}></IonIcon>
+            </IonButton>
+            <IonButton className="text-xs" color="tertiary">
+              AGENDAR
+              <IonIcon slot="start" icon={calendarOutline}></IonIcon>
+            </IonButton>
+          </div>
+        </div>
+      </IonCard>
+    </div>
+  );
+}
+
+export default DoctorCard;
