e33e0e0da2827c152be65ac7d1b61ceb1634b5cf
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-12-09 18:23:08 -0300

    Update ListDoctor.tsx

--- src/components/Pacient/ListDoctor/ListDoctor.tsx
+++ src/components/Pacient/ListDoctor/ListDoctor.tsx
@@ -8,6 +8,7 @@ import {
   IonCardContent,
   IonIcon,
   IonImg,
+  IonItem,
   IonList,
   IonThumbnail,
   useIonAlert,
@@ -19,71 +20,24 @@ import {
   heartOutline,
   watch,
 } from "ionicons/icons";
-import { useState } from "react";
-
-const slideOpts = {
-  initialSlide: 1,
-  speed: 400,
-};
+import { mockedDoctors } from "../../../mocks/doctor";
+import DoctorCard from "../../Doctor/DoctorCard/DoctorCard";
 
 const ListDoctor: React.FC = () => {
-  const [change, setChange] = useState<boolean>(false);
-  const [_class, setClass] = useState<string>("flex hidden");
-  const [presentAlert] = useIonAlert();
-  const [handlerMessage, setHandlerMessage] = useState("");
-  const [roleMessage, setRoleMessage] = useState("");
-  const [present] = useIonToast();
-  const presentToast = () => {
-    present({
-      message: "Consulta cancelada com sucesso",
-      duration: 1500,
-      position: "top",
-    });
-  };
-
-  const showChat = () => {
-    setChange(!change);
-    if (change === true) {
-      setClass("flex");
-    } else {
-      setClass("flex hidden");
-    }
-  };
+  
 
   return (
-    <div className="container" onClick={showChat}>
-      <IonCard className="bd-20 card">
-        <IonCardContent className="flex justify-between w-auto">
-          <IonThumbnail slot="start">
-            <img
-              className="min-w-[80px]"
-              alt="Pic-Doctor"
-              src="./assets/avatar/Pic-Doctor.png"
-            />
-          </IonThumbnail>
-          <div className="flex ml-9">
-            <div className="flex flex-col gap-2">
-              <span className="text-black font-bold">Dra. Maria Renata</span>
-              <p className="font-normal">Psicóloga</p>
-              <span className="font-medium">98 Avaliações</span>
-            </div>
-          </div>
-          <IonImg src="./assets/icon/Logo.svg"></IonImg>
-        </IonCardContent>
-        <div className="flex flex-row justify-center items-center">
-          <div className={_class}>
-            <IonButton className="text-xs w-max">
-              ABRIR CHAT
-              <IonIcon slot="start" icon={chatbubbleOutline}></IonIcon>
-            </IonButton>
-            <IonButton className="text-xs"  color="tertiary">
-              AGENDAR
-              <IonIcon slot="start" icon={calendarOutline}></IonIcon>
-            </IonButton>
-          </div>
-        </div>
-      </IonCard>
-    </div>
+    <IonList>
+      {mockedDoctors.map((element) => (
+      <DoctorCard
+        doctor={element}
+        key={element.id}
+      />
+    ))}
+   
+    </IonList>
+    
+    
   );
 };
 
