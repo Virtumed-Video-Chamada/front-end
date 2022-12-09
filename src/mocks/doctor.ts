59aa952652204e81db3b313df4d11a4ca9421462
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-12-09 18:19:48 -0300

    implement mocked data for testing

---
+++ src/mocks/doctor.ts
@@ -0,0 +1,52 @@
+import * as T from "../types/index";
+
+export const mockedDoctors: T.Doctor[] = [
+  {
+    id: "b329050a-e2a6-4603-988a-a0235e923a77",
+    email: "user@email.com",
+    nameDoctor: "Jorge",
+    crm: "CRM/SP 123456",
+    cpf: "13954075938",
+    speciality: "Nutricionista",
+  },
+  {
+    id: "b11329050a-e2a6-4603-988a-a0235e923a77",
+    email: "daniel@email.com",
+    nameDoctor: "Daniel",
+    crm: "CRM/SP 4444",
+    cpf: "45996262633",
+    speciality: "Pediatra",
+  },
+  {
+    id: "b111329050a-e2a6-4603-988a-a0235e923a77",
+    email: "daniel@email.com",
+    nameDoctor: "Daniel",
+    crm: "CRM/SP 4444",
+    cpf: "45996262633",
+    speciality: "Pediatra",
+  },
+  {
+    id: "b12221329050a-e2a6-4603-988a-a0235e923a77",
+    email: "daniel@email.com",
+    nameDoctor: "Daniel",
+    crm: "CRM/SP 4444",
+    cpf: "45996262633",
+    speciality: "Pediatra",
+  },
+  {
+    id: "b1331329050a-e2a6-4603-988a-a0235e923a77",
+    email: "daniel@email.com",
+    nameDoctor: "Daniel",
+    crm: "CRM/SP 4444",
+    cpf: "45996262633",
+    speciality: "Pediatra",
+  },
+  {
+    id: "44-e2a6-4603-988a-a0235e923a77",
+    email: "daniel@email.com",
+    nameDoctor: "Daniel",
+    crm: "CRM/SP 4444",
+    cpf: "45996262633",
+    speciality: "Pediatra",
+  },
+];
