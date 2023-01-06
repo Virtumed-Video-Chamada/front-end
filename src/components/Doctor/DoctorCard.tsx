269f30bec4c8b0db597695616a0ab71041735348
Thaís Monteiro <thais.vmonteiro@gmail.com>
2023-01-06 14:58:09 -0300

    ajustes estilos

--- src/components/Doctor/DoctorCard.tsx
+++ src/components/Doctor/DoctorCard.tsx
@@ -51,7 +51,7 @@ function DoctorCard({ doctor }: DoctorCardProps) {
 
   return (
     <div  onClick={showChat} >
-      <IonCard className="bd-20" >
+      <IonCard className="bd-20 cardDoctorWhite">
         <IonCardContent className="flex justify-between">
           <IonThumbnail slot="start">
             <img
