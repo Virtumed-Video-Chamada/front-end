d2eba974b4f7014821d2acadd100593d3e01b43c
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2023-01-19 14:58:18 -0300

    (fix): correção de CSS (quickaccess Admin - Doctor e Patient)

--- src/components/Admin/DashboardAdmin/DashboardAdmin.tsx
+++ src/components/Admin/DashboardAdmin/DashboardAdmin.tsx
@@ -8,12 +8,14 @@ import {
   IonCardHeader,
   IonCardSubtitle,
   IonCardTitle,
+  IonCol,
   IonGrid,
   IonImg,
   IonLabel,
   IonRow,
   IonSlide,
   IonSlides,
+  IonText,
   IonThumbnail,
   IonTitle,
 } from "@ionic/react";
@@ -23,89 +25,71 @@ const DashboardAdmin: React.FC = () => {
   return (
     <div className="container">
       <IonGrid>
-        
-        <IonRow className="flex flex-nowrap  mb-0">
-          <IonCard className="mr-1 bd-20 quickCard" routerLink="/register-doctor">
+        <IonCol className="justify-center">
+          <IonText class=" flex justify-center text-black text-xl font-bold">
+            Médico
+          </IonText>
+          <IonCard className=" bd-20 buttonCard" routerLink="/register-doctor">
             <IonImg
               className="imgCard mx-auto"
-              
               src="./assets/icon/medical-equipment.svg"
             ></IonImg>
-            <p className="text-center textCard">Cadastrar Médico</p>
+            <p className="text-center ">Cadastrar Médico</p>
           </IonCard>
-          <IonCard className="mr-1 bd-20 quickCard" routerLink="/find-doctor">
+          <IonCard className="bd-20 buttonCard" routerLink="/find-doctor">
             <IonImg
-            color="primary"
+              color="primary"
               className="imgCard mx-auto"
               src="./assets/icon/medic.svg"
             ></IonImg>
-            <p className="text-center textCard">Médicos cadastrados</p>
+            <p className="text-center ">Médicos cadastrados</p>
           </IonCard>
-          <IonCard
-            className="mr-1 bd-20 quickCard"
-            routerLink="/link-doctor"
-          >
-            <IonImg
-              className="imgCard mx-auto"
-              src="./assets/icon/link.svg"
-            ></IonImg>
-            <p className="text-center textCard">Vincular Médico</p>
-          </IonCard>
-        </IonRow>
-        <IonRow className="flex flex-nowrap mb-0">
-          <IonCard className="mr-1 bd-20 quickCard" routerLink="/register-patient">
+        </IonCol>
+        <IonCol>
+          <IonText class=" flex justify-center text-black text-xl font-bold">
+            Paciente
+          </IonText>
+          <IonCard className=" bd-20 buttonCard" routerLink="/register-patient">
             <IonImg
               className="imgCard mx-auto"
               src="./assets/icon/user.svg"
             ></IonImg>
-            <p className="text-center textCard">Cadastrar Paciente</p>
+            <p className="text-center ">Cadastrar Paciente</p>
           </IonCard>
-          <IonCard className="mr-1 bd-20 quickCard" routerLink="/patient-list">
+          <IonCard className=" bd-20 buttonCard" routerLink="/patient-list">
             <IonImg
               className="imgCard mx-auto"
               src="./assets/icon/users.svg"
             ></IonImg>
-            <p className="text-center textCard">Pacientes Cadastrados</p>
+            <p className="text-center ">Pacientes Cadastrados</p>
           </IonCard>
-          <IonCard
-            className="mr-1 bd-20 quickCard"
-            routerLink="/link-patient"
-          >
-            <IonImg
-              className="imgCard mx-auto"
-              src="./assets/icon/link.svg"
-            ></IonImg>
-            <p className="text-center textCard">Vincular Paciente</p>
-          </IonCard>
-       
-        </IonRow>
-        <IonRow className="flex flex-nowrap mb-0">
-          <IonCard className="mr-1 bd-20 quickCard" routerLink="/register-clinic">
+        </IonCol>
+        <IonCol>
+          <IonText class=" flex justify-center text-black text-xl font-bold">
+            Clínica
+          </IonText>
+          <IonCard className=" bd-20 buttonCard" routerLink="/register-clinic">
             <IonImg
               className="imgCard mx-auto"
               src="./assets/icon/hospital.svg"
             ></IonImg>
-            <p className="text-center textCard ">Cadastrar Clínica</p>
+            <p className="text-center  ">Cadastrar Clínica</p>
           </IonCard>
-          <IonCard className="mr-1 bd-20 quickCard" routerLink="/clinic-list">
+          <IonCard className=" bd-20 buttonCard" routerLink="/clinic-list">
             <IonImg
               className="imgCard mx-auto"
               src="./assets/icon/health.svg"
             ></IonImg>
-            <p className="text-center textCard">Clínicas Cadastradas</p>
+            <p className="text-center ">Clínicas Cadastradas</p>
           </IonCard>
-          <IonCard
-            className="mr-1 bd-20 quickCard"
-            routerLink="historical-clinic"
-          >
+          <IonCard className=" bd-20 buttonCard" routerLink="historical-clinic">
             <IonImg
               className="imgCard mx-auto"
               src="./assets/icon/historical.svg"
             ></IonImg>
-            <p className="text-center textCard">Consultar Exames</p>
+            <p className="text-center ">Consultar Exames</p>
           </IonCard>
-         
-        </IonRow>
+        </IonCol>
       </IonGrid>
     </div>
   );
